664d0022dcc59b4fbc8c55abce2720a8383b6073
Eric Yager <eric@northcountryengineer.com>
2023-06-02 14:31:13 -0400

    Updated header to have campaign banner centered. Updated Home to be blank. Added a Banner image and a Family image. Family image will be incorporated onto the homepage in the next commit.

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { AppBar, Toolbar, Drawer, Typography, Grid } from '@mui/material';
 
-
 export const Header = ({title}) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -11,19 +10,31 @@ export const Header = ({title}) => {
 
   return (
     <>
-        <AppBar position="fixed" sx={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        <AppBar position="fixed" sx={{ height: '200px', backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
             <Toolbar>
-                <Grid container spacing={1}>
-                    <Grid item xs={24}>
-                        <Typography variant="h4">{title}</Typography>
-                    </Grid>
-                    <Grid item xs={12} >
-                        Vote for Dekalb, Vote for Eric
+                <Grid container spacing={1} justifyContent="center">
+                    <Grid item xs={12} sx={{height:'100px'}} />
+                    <Grid item xs={12} sm={6} md={4}>
+                        <div style={{ 
+                            width: '400px;',
+                            height: 'auto', 
+                            position: 'relative' 
+                        }}>
+                            <img 
+                                src="/Images/Banner.png"
+                                alt="Banner"
+                                style={{ 
+                                    width: '100%', 
+                                    height: 'auto', 
+                                    position: 'absolute', 
+                                    top: '50%', 
+                                    left: '50%', 
+                                    transform: 'translate(-50%, -50%)' 
+                                }} 
+                            />
+                        </div>
                     </Grid>
                 </Grid>
-
-
-                
             </Toolbar>
         </AppBar>
         <Drawer anchor="left" open={isMenuOpen} onClose={toggleMenu}>
--- src/Views/Home/Home.tsx
+++ src/Views/Home/Home.tsx
@@ -1,27 +1,32 @@
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, Grid, Container } from "@mui/material";
 import { Calendar } from "./Components";
 
 export function Home() {
   return (
-    <div>
-         <CssBaseline />
-        <img 
-            src="/Images/Wallpaper.png"  
-            alt="Wallpaper" 
-            style={{ 
-                width: '100%', 
-                height: '100%',
-                position: 'absolute', 
-                zIndex: '-1'
-            }} 
+    <div style={{ 
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      width: '100vw', 
+      height: '100vh', 
+      backgroundImage: 'url(/Images/Wallpaper.png)', 
+      backgroundSize: 'cover', 
+      backgroundPosition: 'center',
+      zIndex: '-1'
+    }}>
+      <CssBaseline />
+
+        <Grid container spacing={3}>
+          <Grid item xs={12} style={{ height: '250px' }}></Grid>
+
+          <Grid item xs={12}>
             
-        />
+          </Grid>
 
-        <div style={{ height: '180px' }}></div>
-
-        <div>
+          <Grid item xs={12}>
             <Calendar />
-        </div>
+          </Grid>
+        </Grid>
     </div>
   );
 }
